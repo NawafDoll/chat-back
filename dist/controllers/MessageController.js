2334688be3d3da7ae040790bee2e31113f08671b
Nawaf <nawaftalaat@hotmail.com>
2023-06-22 22:53:14 +0300

    dew

--- dist/controllers/MessageController.js
+++ dist/controllers/MessageController.js
@@ -65,6 +65,7 @@ const sendfile = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         // }
         const result = yield Claudinary_1.cloudinary.v2.uploader.upload(image, {
             folder: "products",
+            format: "auto",
         });
         var newMessage = {
             sender: user_id.id,
